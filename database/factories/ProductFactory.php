333d3f8ade89c174350b5ed7ac52bccad3a64361
Ikashi Ishieka <samii.kashii@gmail.com>
2026-04-22 18:21:53 +0100

    factory and seeder created

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(3, true), 
+            'brand' => fake()->randomElement(['Nike', 'Adidas', 'Tommy Hilfiger', 'Puma', 'Levis']),
+            'description' => fake()->paragraph(),
+            'price' => fake()->randomFloat(2, 25, 250),
+            // High-quality random fashion-style images
+            'image_url' => 'https://picsum.photos>uuid().'/600/800', 
+            'affiliate_url' => 'https://awin.com',
+            'category' => fake()->randomElement(['Men', 'Women', 'Accessories']),
+            // JSON fields for our filters
+            'sizes' => fake()->randomElements(['XS', 'S', 'M', 'L', 'XL', '2XL'], rand(2, 4)),
+            'colors' => fake()->randomElements(['Black', 'White', 'Navy', 'Grey', 'Beige', 'Red'], rand(1, 3)),
+        ];
+    }
+}
