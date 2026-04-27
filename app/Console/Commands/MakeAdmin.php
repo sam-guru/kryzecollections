116a35760549030392f87ed42ef7371820895528
Ikashi Ishieka <samii.kashii@gmail.com>
2026-04-27 16:55:02 +0100

    makeAdmin.php edited

--- app/Console/Commands/MakeAdmin.php
+++ app/Console/Commands/MakeAdmin.php
@@ -15,6 +15,18 @@ class MakeAdmin extends Command
      */
     public function handle()
     {
+
+        \App\Models\Product::all()->each(function ($product) {
+            if (str_contains($product->main_image, 'storage/')) {
+                $product->main_image = str_replace('storage/', '', $product->main_image);
+                $product->save();
+            }
+        });
+
+        $this->info('Database paths cleaned successfully!');
+
+
+        
         //
         $user = \App\Models\User::where('email', 'admin@gmail.com')->first();
         
