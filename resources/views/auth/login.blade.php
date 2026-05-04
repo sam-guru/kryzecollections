cd72ed918630576f60fc683bc9791e77491e8536
Ikashi Ishieka <samii.kashii@gmail.com>
2026-05-04 08:18:39 +0100

    login page updated2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
             <svg class="w-4 h-4 me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
-            {{ __('Back to Homepage') }}
+            {{ __(' Back to Homepage') }}
         </a>
     </div>
 
